5e4be91297dd468108230b44d9139518bda4c495
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-19 08:49:01 -0700

    add axios call

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,13 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+    .get('https://swapi.co/api/people')
+    .then((res) => {
+      console.log(res);
+    })
+  }, []);
 
   return (
     <div className="App">
